cbcb59b68941456b92fc65fb6f47b231b3088f72
Yasuhisa Yoshida <syou6162@gmail.com>
2013-08-28 16:06:59 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject multi-class-perceptron "0.0.1"
+  :description "Simple multi-class perception implementation written in clojure"
+  :url "http://github.com/syou6162/multi-class-perceptron/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
